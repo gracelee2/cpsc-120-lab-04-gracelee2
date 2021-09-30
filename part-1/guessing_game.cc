ec25c9c63e84a1a754783063e661a1f9c8e5df48
Grace <grace1@csu.fullerton.edu>
2021-09-30 15:46:06 -0700

    done

--- part-1/guessing_game.cc
+++ part-1/guessing_game.cc
@@ -28,40 +28,22 @@ int main(int argc, char const *argv[]) {
     int difference_guess = kSecretNumber - guess;
     int difference_last_guess = kSecretNumber - last_guess;
     last_guess = guess;
-   if (guess == kSecretNumber){
-    cout << "\tHooray! You guessed the secret number!!\n";
-    cout << "Do you want to play again? (y or n)>";
-    cin >> answer;
-    if(answer == "n"){
-      is_game_over = true;
-    } else if(answer == "y"){
-      is_game_over = false;
+    if (guess == kSecretNumber) {
+      cout << "\tHooray! You guessed the secret number!!\n";
+      cout << "Do you want to play again? (y or n)>";
+      cin >> answer;
+      if (answer == "n") {
+        is_game_over = true;
+      } else if (answer == "y") {
+        is_game_over = false;
+      }
+
+    } else if (difference_guess < difference_last_guess) {
+      cout << "\tYou're getting warmer.\n";
+    } else {
+      cout << "\tYou're getting colder.\n";
     }
-
-
-  } else if(difference_guess < difference_last_guess){
-    cout <<"\tYou're getting warmer.\n";
-  } else {
-    cout <<"\tYou're getting colder.\n";
   }
-
-}
-
-  // TODO: Write an loop that will continue until the player decides to quit
-  // TODO: Inside the loop, prompt the user for their guess
-  // for example     cout << "What's your guess?> ";
-  // TODO: Store the input from the keyboard into the variable guess
-  // TODO: Check to see if guess has the same value as kSecretNumber
-  //    If so, then the player has won and they can play again
-  //    Prompt the user if they would like to continue playing. If the
-  //    answer is "n" then exit the game. Otherwise, start a new game.
-  // TODO: Else, the player didn't win so they need to know if they are
-  //    getting warmer or colder.
-  //    Check to see if kSecretNumber - guess is less than kSecretNumber -
-  //    last_guess If it is, print you're getting warmer. Otherwise, print
-  //    you're getting colder.
-  // TODO: At the end of every iteration of the loop, save the current guess
-  // into the variable last_guess.
 
   return 0;
 }
