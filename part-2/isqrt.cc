1ad8d383b4e8da6d20e0a15e5dd3295a18d230ec
Grace <grace1@csu.fullerton.edu>
2021-09-30 14:04:11 -0700

    halfway

--- part-2/isqrt.cc
+++ part-2/isqrt.cc
@@ -1,4 +1,12 @@
-
+// Grace Lee
+// CPSC 120-01
+// 2021-09-30
+// grace1@csu.fullerton.edu
+// @gracelee2
+//
+// Lab 04-02
+//
+// Lab that calculates integer square roots
 #include <iostream>
 
 using namespace std;
@@ -9,8 +17,15 @@ int main(int argc, char const* argv[]) {
   int input_number = 0;
   cin >> input_number;
   int square_root = 0;
+  int initial_estimate = input_number/2;
+  if(initial_estimate == 0){
+    input_number = square_root;
+  }else{
+    int next_estimate = initial_estimate + input_number;
+  }
 
-  // TODO: Use Heron's method as described in the README to calculate the 
+
+  // TODO: Use Heron's method as described in the README to calculate the
   // integer square root of input_number and store it in square_root.
   // Use the information in the README.md to write the loop
   // using the pseudocode algorithm.
